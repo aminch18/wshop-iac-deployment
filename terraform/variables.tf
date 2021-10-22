f8921e27ce9df5e67b76ebbb6cd857f71c15872e
Joan Jané Ballester <jjane@plainconcepts.com>
2021-10-22 13:26:38 +0200

    Fix kv length

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -30,7 +30,7 @@ variable "sql_database_name" {
 variable "key_vault_name" {
     type        = string
     description = "Azure Resource Group Name."
-    default = "kv-gh-actions-wshop-jj"
+    default = "kv-gh-act-wshop-jj"
 }
 
 variable "location" {
